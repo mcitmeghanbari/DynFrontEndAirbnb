045ede413a13bd23a27bcc50b5655158c38c93c6
Mohammad Ebrahim Ghanbari <meghanbari@hotmail.com>
2020-04-20 00:29:08 -0400

    mcit project - component2 - meg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//--- header
+import { HeaderComponent } from './header/header.component';
+import { HeaderNavbarComponent } from './header/header-navbar/header-navbar.component';
+//----------------------------------------------------------------------------------------
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderNavbarComponent
   ],
   imports: [
     BrowserModule,
